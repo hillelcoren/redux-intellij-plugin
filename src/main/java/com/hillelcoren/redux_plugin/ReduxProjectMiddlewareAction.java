045f945e2f230ccff0f181069ddc95b5958196bf
Hillel Coren <hillelcoren@gmail.com>
2018-10-30 18:27:32 +0200

    Support context menu actions

--- src/main/java/com/hillelcoren/redux_plugin/ReduxProjectMiddlewareAction.java
+++ src/main/java/com/hillelcoren/redux_plugin/ReduxProjectMiddlewareAction.java
@@ -1,17 +1,7 @@
 package com.hillelcoren.redux_plugin;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.editor.CaretModel;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.ScrollType;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
-
-import java.util.ArrayList;
 
 public class ReduxProjectMiddlewareAction extends ReduxProjectAction {
 
